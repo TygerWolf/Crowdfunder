4f0d10ffa6226a476eb4220edd1eb6625aa1ac42
Sandeep Dhanjani <sandeepdhanjani@gmail.com>
2014-05-06 15:12:44 -0400

    reboot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crowdfunder::Application.config.secret_key_base = 'da33540d4c83ce31e3024a6aae13b4a1320aa9050ca513ac25a853fff8d99c72559c321fde3280b94797fd216424e61d0bbc7733dbee09d6c0edeae827245f22'
